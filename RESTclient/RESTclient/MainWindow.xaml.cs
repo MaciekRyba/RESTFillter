a5c98a944c4cca15d6a03f01ddf682b8996eee8a
MaciekRyba <49067407+MaciekRyba@users.noreply.github.com>
2020-12-15 10:52:16 +0100

    RESTfilters

---
+++ RESTclient/RESTclient/MainWindow.xaml.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RESTclient.Models;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Net;
+using System.Web;
+
+namespace RESTclient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        static HttpClient client = new HttpClient();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            client.BaseAddress = new Uri("https://localhost:44378/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        
+
+        private async void bAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Persons person = new Persons();
+            person = SetPersonsValue();
+
+            var url = await CreateProductAsync(person);
+           
+        }
+
+        static async Task<Uri> CreateProductAsync(Persons person)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "api/listitems/", person);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        static async Task<List<Persons>> GetAllPersonsAsync()
+        {
+            List<Persons> persons = null;
+
+            HttpResponseMessage response = await client.GetAsync(("api/listitems/"));
+            if (response.IsSuccessStatusCode)
+            {
+                persons = await response.Content.ReadAsAsync<List<Persons>>();
+            }
+            return persons;
+        }
+
+        public Persons SetPersonsValue()
+        {
+            Persons persons = new Persons();
+            Pets pet = new Pets();
+            Cars car = new Cars();
+
+            persons.name = tName.Text;
+            
+            persons.surname = tSurname.Text;
+
+            persons.city = tCity.Text;
+
+
+            if (tYear.Text != "")
+            {
+                persons.year = int.Parse(tYear.Text);
+            }
+            else
+            {
+                persons.year = 0;
+            }
+
+
+            car.theCarBrand = tBrand.Text;
+            car.color = tColor.Text;
+
+            if (tCarYer.Text != "")
+            {
+                car.year = int.Parse(tCarYer.Text);
+            }
+            else
+                car.year = 0;
+
+            if (tMileage.Text != "")
+            {
+                car.mileage = int.Parse(tMileage.Text);
+            }
+            else
+                car.mileage = 0;
+
+            pet.animal = tAnimal.Text;
+            pet.name = tAnimalName.Text;
+            if (tAnimalYear.Text != "")
+            {
+                pet.year = int.Parse(tAnimalYear.Text);
+            }
+            else
+                pet.year = 0;
+
+            persons.car = car;
+            persons.pet = pet;
+
+
+            return persons;
+        }
+
+        static async Task<Persons> GetPersonAsync(string path)
+        {
+            Persons product = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                product = await response.Content.ReadAsAsync<Persons>();
+            }
+            return product;
+        }
+
+
+        static async Task<Persons> UpdatePersonAsync(Persons person)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync(
+                $"api/listitems/{person.id}", person);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated product from the response body.
+            person = await response.Content.ReadAsAsync<Persons>();
+            return person;
+        }
+
+        static async Task<HttpStatusCode> DeletePersonAsync(string id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync(
+                $"api/listitems/{id}");
+            return response.StatusCode;
+        }
+
+
+        public async void bGetAll_Click(object sender, RoutedEventArgs e)
+        {
+
+            List<Persons> persons = await GetAllPersonsAsync();
+            AddValueToListBox(persons);
+            
+
+        }
+
+       public void AddValueToListBox(List<Persons> persons)
+        {
+                personsList.ItemsSource = persons;
+        }
+
+        public int GetValueIdfromListBox()
+        {
+            try
+            {
+                Persons persons = personsList.SelectedItem as Persons;
+                return persons.id;
+            }catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return 0;
+        }
+        public void  SetValuesToTexBoxs(Persons person)
+        {
+              tName.Text = person.name;
+              tSurname.Text = person.surname;
+              tCity.Text = person.city;
+              tYear.Text = person.year.ToString();
+
+           
+              tBrand.Text = person.car.theCarBrand;
+              tColor.Text = person.car.color;
+              tCarYer.Text = person.car.year.ToString();
+              tMileage.Text = person.car.mileage.ToString();
+
+
+              tAnimal.Text = person.pet.animal;
+              tAnimalName.Text = person.pet.name;
+              tAnimalYear.Text= person.pet.year.ToString();
+        }
+
+        private async void bGetOne_Click(object sender, RoutedEventArgs e)
+        {
+            int idValue = GetValueIdfromListBox();
+            if (idValue != 0)
+            {
+                string path = "api/listitems/" + idValue.ToString();
+                MessageBox.Show(path);
+                Persons person = await GetPersonAsync(path);
+                SetValuesToTexBoxs(person);
+            }
+        }
+
+        private async void bEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+            int idValue = GetValueIdfromListBox();
+            if (idValue != 0)
+            {
+                Persons person = SetPersonsValue();
+                person.id = idValue;
+
+                Persons updatedPerson = await UpdatePersonAsync(person);
+                SetValuesToTexBoxs(updatedPerson);
+
+                List<Persons> persons = await GetAllPersonsAsync();
+                AddValueToListBox(persons);
+            }
+        }
+
+        private async void bDelet_Click(object sender, RoutedEventArgs e)
+        {
+            int idValue = GetValueIdfromListBox();
+            if (idValue != 0)
+            {
+                var statusCode = await DeletePersonAsync(idValue.ToString());
+
+                List<Persons> persons = await GetAllPersonsAsync();
+                AddValueToListBox(persons);
+            }
+        }
+
+        private async void bFilter_Click(object sender, RoutedEventArgs e)
+        {
+           
+            string path = "api/Persons?";
+
+            var query = HttpUtility.ParseQueryString(string.Empty);
+                query["name"] = tFiltrName.Text;
+                query["city"] = tFiltrCity.Text;
+                query["year"] = tFiltrYear.Text;
+                query["contains"]= cbContains.IsChecked.ToString();
+                query["lettersize"] = cbLetterSize.IsChecked.ToString();
+
+            //query["lowercase"] = lowercaseCheckBox.IsChecked.ToString();
+           // query["contains"] = containsCheckBox.IsChecked.ToString();
+            string queryString = path+query.ToString();
+
+            List<Persons> persons = null;
+
+            HttpResponseMessage response = await client.GetAsync((queryString));
+
+            if (response.IsSuccessStatusCode)
+            {
+                persons = await response.Content.ReadAsAsync<List<Persons>>();
+                AddValueToListBox(persons);
+
+            }
+            else
+            {
+                MessageBox.Show(await response.Content.ReadAsStringAsync(), "", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
+        }
+        }
+    }
+
---
+++ RestService/RestService/Controllers/ListItemsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RestService.Models;
+
+namespace RestService.Controllers
+{
+    public class ListItemsController : ApiController
+    {
+        private static List<Persons> lstPersons { get; set; } = new List<Persons>() { 
+            new Persons(){id = 1,name = "Maciej", surname = "Rybarczuk",year=1990, city = "Siemiatycze", pet = new Pets{name = "Rocky", animal = "Pies", year = 2015 }, car = new Cars{ theCarBrand = "Audi", year = 2000, color = "Czerwony", mileage = 10000} },
+            new Persons(){id = 2,name = "Ala", surname = "Kot",city="Bialystok",year=1997, pet= new Pets{name = "Alfa", animal = "Kot", year = 2018 }, car = new Cars{ theCarBrand = "BMW", year = 2010, color = "Czarny", mileage = 10000}},
+            new Persons(){id = 3,name = "Ala", surname = "Nowak",city="Krakow",year=1997, pet= new Pets{name = "Max", animal = "Papuga", year = 2015 }, car = new Cars{ theCarBrand = "Fiat", year = 1998, color = "Czerwony", mileage = 10000}}
+        };
+        // GET api/<controller>
+        public IEnumerable<Persons> Get()
+        {
+            return lstPersons;
+        }
+
+        public HttpResponseMessage Get(int id)
+        {
+            var item = lstPersons.FirstOrDefault(x => x.id == id);
+            if (item != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        [Route("api/Persons/{name?}/{city?}/{year?}/{contains?}/{lettersize?}")]
+        public HttpResponseMessage GetByName(string name = null, string city = null, int ? year = null, bool contains = false, bool lettersize  = false)
+        {
+            var item = lstPersons.AsEnumerable();
+
+            if(year!=null)
+                item= item.Where(x => x.year == year);
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                if(contains)
+                     item = item.Where(x => (lettersize ? x.name.ToLower().Contains(name.ToLower()) : x.name.Contains(name)));
+                else
+                    item = item.Where(x => (lettersize ? x.name.ToLower() == name.ToLower() : x.name == name));
+            }
+
+            if (!string.IsNullOrEmpty(city))
+            {
+                if (contains)
+                    item = item.Where(x => (lettersize ? x.city.ToLower().Contains(city.ToLower()) : x.city.Contains(city)));
+                else
+                    item = item.Where(x => (lettersize ? x.city.ToLower() == city.ToLower() : x.city == city));
+            }
+
+            if (item != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        // POST api/<controller>
+        public HttpResponseMessage Post([FromBody] Persons model)
+        {
+            if (string.IsNullOrEmpty(model?.name) || string.IsNullOrEmpty(model?.surname))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            var maxId = 0;
+
+            if (lstPersons.Count > 0)
+            {
+                maxId = lstPersons.Max(x => x.id);
+            }
+            model.id = maxId + 1;
+            lstPersons.Add(model);
+            return Request.CreateResponse(HttpStatusCode.Created, model);
+        }
+
+        // PUT api/<controller>/5
+        public HttpResponseMessage Put(int id, [FromBody] Persons model)
+        {
+            if (string.IsNullOrEmpty(model?.name) || string.IsNullOrEmpty(model?.surname))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            var item = lstPersons.FirstOrDefault(x => x.id == id);
+
+            if (item != null)
+            {
+                // Update *all* of the item's properties
+                item.name = model.name;
+                item.surname = model.surname;
+                item.city = model.city;
+                item.year = model.year;
+
+                item.pet.animal = model.pet.animal;
+                item.pet.name = model.pet.name;
+                item.pet.year = model.pet.year;
+
+                item.car.theCarBrand = model.car.theCarBrand;
+                item.car.color = model.car.color;
+                item.car.year = model.car.year;
+                item.car.mileage = model.car.mileage;
+
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        // DELETE api/<controller>/5
+        public HttpResponseMessage Delete(int id)
+        {
+            var item = lstPersons.FirstOrDefault(x => x.id == id);
+            if (item != null)
+            {
+                lstPersons.Remove(item);
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+    }
+}
